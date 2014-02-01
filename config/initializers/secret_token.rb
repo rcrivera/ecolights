21cd81767d3076247c2a33a8caa3693c539325a7
Roberto C. Rivera <rivera.roberto89@gmail.com>
2014-01-31 21:35:02 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecolights::Application.config.secret_key_base = 'ac54d924ff0b8b8b08355689c69697c35e0367d25e35bd8885740689bc07479027a5d8bf93d7429568cdc524732367553c6332f93187e8eb7ec9961763a595fe'
